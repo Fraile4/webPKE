82cb6018e1007e7dc8b6d0c68b7af4bb87ee0f7f
Unai <unaifrailegarraza@gmail.com>
2023-11-07 14:59:32 +0100

    menu

--- JS/menu.js
+++ JS/menu.js
@@ -1,7 +1,12 @@
-
-const openBtn = document.querySelector('.open-btn');
-const menu = document.querySelector('.menu');
-
-openBtn.addEventListener('click', () => {
-  menu.classList.toggle('open');
-});
+document.addEventListener("DOMContentLoaded", function() {
+    const menu = document.getElementById("menu");
+    const mostrarMenuButton = document.getElementById("openBtn");
+  
+    mostrarMenuButton.addEventListener("click", function() {
+      if (parseInt(getComputedStyle(menu).left) === 0) {
+        menu.style.left = "-200px"; // Oculta el menú hacia la izquierda
+      } else {
+        menu.style.left = "0"; // Muestra el menú
+      }
+    });
+  });
