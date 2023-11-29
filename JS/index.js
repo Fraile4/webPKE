1042245cb6eddd22f5a0d30d62a9eeedb1d6af4b
ioritztubio <itubio002@ikasle.ehu.eus>
2023-11-29 13:29:55 +0100

    update

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,12 @@
 document.querySelector('nav > button').addEventListener('click', function() {
   var navbar = document.querySelector('.navbar');
+  var izenburua = document.querySelector('.Izenburua')
   if (navbar.style.backgroundColor === 'transparent') {
     navbar.style.backgroundColor = 'black';
+    izenburua.style.display = 'none';
   } else if(navbar.style.backgroundColor === 'black'){
     navbar.style.backgroundColor = 'transparent';
+    izenburua.style.display = 'block';
   }
 });
  
