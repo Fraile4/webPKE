2285ee6d7c3f4d0d7a0c0443c25d358b6a472254
ManexOrmazabal <mormazabal021@ikasle.ehu.eus>
2023-12-04 15:44:20 +0100

    a

--- JS/egutegiaUrtea.js
+++
@@ -1,9 +0,0 @@
-        let currentYear = 2023;
-
-        function changeYear(offset) {
-            currentYear += offset;
-            document.getElementsByTagName('th')[0].innerHTML = currentYear;
-
-            // Puedes realizar aquí cualquier otra lógica relacionada con cambiar de año
-        }
-    
